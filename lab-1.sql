00b4ed43d0dd45b37d2fceaa00380610c794e3af
Karol Kadziola <karkad96@gmail.com>
2020-10-06 17:59:38 +0200

    Added remaining exercises from points 6 and 7 and also slightly changed queries

--- lab-1.sql
+++ lab-1.sql
@@ -3,12 +3,6 @@ CREATE DATABASE s283109;
 \c s283109
 --1
 
-drop table wynagrodzenia;
-drop table godziny;
-drop table pracownicy;
-drop table pensja_stanowisko;
-drop table premia;
-
 --2
 CREATE SCHEMA firma;
 SET search_path TO firma;
@@ -29,15 +23,18 @@ GRANT ksiegowosc TO dummy;
 
 --4
 	--a)
-	CREATE TABLE pracownicy(id_pracownika SERIAL, imie VARCHAR(50) NOT NULL, nazwisko VARCHAR(50) NOT NULL, adres VARCHAR(50) NOT NULL, telefon VARCHAR(9));
+	CREATE TABLE pracownicy(id_pracownika SERIAL, imie VARCHAR(50) NOT NULL, nazwisko VARCHAR(50) NOT NULL,
+	adres VARCHAR(50) NOT NULL, telefon VARCHAR(9));
 
-	CREATE TABLE godziny(id_godziny SERIAL, data DATE NOT NULL, liczba_godzin INTEGER NOT NULL, id_pracownika INTEGER NOT NULL);
+	CREATE TABLE godziny(id_godziny SERIAL, data DATE NOT NULL, liczba_godzin INTEGER NOT NULL,
+	id_pracownika INTEGER NOT NULL);
 
 	CREATE TABLE pensja_stanowisko(id_pensji SERIAL, stanowisko VARCHAR(50) NOT NULL, kwota NUMERIC(10, 2));
 
 	CREATE TABLE premia(id_premii SERIAL, rodzaj VARCHAR(50) NOT NULL, kwota NUMERIC(10, 2));
 
-	CREATE TABLE wynagrodzenia(id_wynagrodzenia SERIAL, data DATE, id_pracownika INTEGER NOT NULL, id_godziny INTEGER NOT NULL, id_pensji INTEGER NOT NULL, id_premii INTEGER NOT NULL);
+	CREATE TABLE wynagrodzenia(id_wynagrodzenia SERIAL, data DATE, id_pracownika INTEGER NOT NULL,
+	id_godziny INTEGER NOT NULL, id_pensji INTEGER NOT NULL, id_premii INTEGER NOT NULL);
 	--a)
 	
 	--b)
@@ -52,27 +49,32 @@ GRANT ksiegowosc TO dummy;
 	ALTER TABLE godziny 
 	ADD CONSTRAINT constraint_godziny_pracownik 
 	FOREIGN KEY (id_pracownika) 
-	REFERENCES pracownicy (id_pracownika);
+	REFERENCES pracownicy (id_pracownika)
+	ON DELETE CASCADE;
 
 	ALTER TABLE wynagrodzenia 
 	ADD CONSTRAINT constraint_wynagrodzenia_pracownik
 	FOREIGN KEY (id_pracownika) 
-	REFERENCES pracownicy (id_pracownika);
+	REFERENCES pracownicy (id_pracownika)
+	ON DELETE CASCADE;
 
 	ALTER TABLE wynagrodzenia 
 	ADD CONSTRAINT constraint_wynagrodzania_godzina
 	FOREIGN KEY (id_godziny) 
-	REFERENCES godziny(id_godziny);
+	REFERENCES godziny(id_godziny)
+	ON DELETE CASCADE;
 
 	ALTER TABLE wynagrodzenia 
 	ADD CONSTRAINT constraint_wynagrodzenia_pensja
 	FOREIGN KEY (id_pensji) 
-	REFERENCES pensja_stanowisko (id_pensji);
+	REFERENCES pensja_stanowisko (id_pensji)
+	ON DELETE CASCADE;
 
 	ALTER TABLE wynagrodzenia 
 	ADD CONSTRAINT constraint_wynagrodzenia_premia
 	FOREIGN KEY (id_premii) 
-	REFERENCES premia (id_premii);
+	REFERENCES premia (id_premii)
+	ON DELETE CASCADE;
 	--c)
 	
 	--d)
@@ -122,31 +124,33 @@ VALUES ('Jan',      'Kowalski',   'Wadowicka 19',       '123343456'),
        ('Pawel',    'Broda',      'Rydlowka 53',        '423052450'),
        ('Alicja',   'Czara',      'Krakowska 109',      '227343456'),
        ('Adam',     'Dlugopis',   'Mickiewicza 6',      '520343056'),
-       ('Jessica',  'Jones',      'Mostowa 666',        '923303457');
+       ('Jessica',  'Jones',      'Mostowa 666',        '923303457'),
+       ('Curt',     'Cobain',     'Kaliber 44',         '287246728');
        
 /* 
 	aby dostac numer tygodnia trzeba uzyc: 
-	DATE_PART('year', AGE((select miesiac from godziny where id_godziny=<id>), (select miesiac from godziny where id_godziny=<id>)));
+	SELECT DATE_PART('year', AGE((select numer_tygodnia from godziny where id_godziny=<id>), '1753-01-01'));
 	
 	aby dostac miesiac trzeba uzyc:
 	SELECT EXTRACT(MONTH FROM (select miesiac from godziny where id_godziny=<id>));
 */
 INSERT INTO godziny (data, liczba_godzin, id_pracownika, miesiac, numer_tygodnia) 
-VALUES ('2019-09-10', 5,  2,  '1753-09-01', '1790-01-01'),
-       ('2020-12-17', 4,  3,  '1753-12-01', '1804-01-01'),
-       ('2018-03-06', 12, 10, '1753-03-01', '1763-01-01'),
-       ('2019-01-20', 8,  4,  '1753-01-01', '1756-01-01'),
-       ('2020-02-14', 8,  9,  '1753-02-01', '1760-01-01'),
-       ('2019-11-13', 8,  6,  '1753-11-01', '1799-01-01'),
-       ('2018-05-30', 10, 8,  '1753-05-01', '1775-01-01'),
-       ('2019-10-15', 5,  1,  '1753-10-01', '1795-01-01'),
-       ('2020-06-27', 7,  5,  '1753-06-01', '1779-01-01'),
-       ('2021-04-16', 9,  7,  '1753-04-01', '1753-01-01'),
-       ('2019-08-31', 8,  10, '1753-08-01', '1788-01-01'),
-       ('2020-11-21', 6,  6,  '1753-11-01', '1799-01-01'),
-       ('2020-09-17', 7,  7,  '1753-09-01', '1791-01-01'),
-       ('2019-03-09', 8,  1,  '1753-03-01', '1763-01-01'),
-       ('2020-06-01', 9,  3,  '1753-06-01', '1776-01-01');
+VALUES ('2019-09-10', 115, 2,  '1753-09-01', '1790-01-01'),
+       ('2020-12-17', 167, 3,  '1753-12-01', '1804-01-01'),
+       ('2018-03-06', 125, 10, '1753-03-01', '1763-01-01'),
+       ('2019-01-20', 202, 4,  '1753-01-01', '1756-01-01'),
+       ('2020-02-14', 209, 9,  '1753-02-01', '1760-01-01'),
+       ('2019-11-13', 176, 6,  '1753-11-01', '1799-01-01'),
+       ('2018-05-30', 90,  8,  '1753-05-01', '1775-01-01'),
+       ('2019-10-15', 101, 1,  '1753-10-01', '1795-01-01'),
+       ('2020-06-27', 154, 5,  '1753-06-01', '1779-01-01'),
+       ('2021-04-16', 166, 7,  '1753-04-01', '1753-01-01'),
+       ('2019-08-31', 160, 10, '1753-08-01', '1788-01-01'),
+       ('2020-11-21', 169, 6,  '1753-11-01', '1799-01-01'),
+       ('2020-09-17', 160, 7,  '1753-09-01', '1791-01-01'),
+       ('2019-03-09', 161, 1,  '1753-03-01', '1763-01-01'),
+       ('2020-06-01', 185, 3,  '1753-06-01', '1776-01-01'),
+       ('2019-06-04', 155, 11, '1753-06-01', '1776-01-01');
        
 INSERT INTO pensja_stanowisko (stanowisko, kwota) 
 VALUES ('kurier',        999.45),
@@ -184,7 +188,8 @@ VALUES ('2020-01-15', 1,  15, 12, 3),
        ('2019-02-15', 9,  6,  9,  10),
        ('2020-05-15', 8,  4,  8,  9),
        ('2020-11-15', 5,  11, 1,  1),
-       ('2020-12-15', 4,  5,  4,  5);
+       ('2020-12-15', 4,  5,  4,  5),
+       ('2020-03-15', 11, 16, 2,  5);
 --inserting
 
 --6
@@ -193,11 +198,13 @@ VALUES ('2020-01-15', 1,  15, 12, 3),
 	--a)
 	
 	--b)
-	SELECT id_pracownika FROM wynagrodzenia INNER JOIN pensja_stanowisko ON wynagrodzenia.id_pensji=pensja_stanowisko.id_pensji WHERE pensja_stanowisko.kwota > 1000;
+	SELECT id_pracownika FROM wynagrodzenia, pensja_stanowisko WHERE wynagrodzenia.id_pensji=pensja_stanowisko.id_pensji
+	AND pensja_stanowisko.kwota > 1000;
 	--b)
 	
 	--c)
-	SELECT id_pracownika FROM wynagrodzenia INNER JOIN pensja_stanowisko ON wynagrodzenia.id_pensji=pensja_stanowisko.id_pensji INNER JOIN premia ON wynagrodzenia.id_premii=premia.id_premii WHERE premia.rodzaj='brak' AND pensja_stanowisko.kwota>2000;
+	SELECT id_pracownika FROM wynagrodzenia, pensja_stanowisko, premia WHERE wynagrodzenia.id_pensji=pensja_stanowisko.id_pensji
+	AND wynagrodzenia.id_premii=premia.id_premii AND premia.rodzaj='brak' AND pensja_stanowisko.kwota > 2000;
 	--c)
 	
 	--d)
@@ -209,45 +216,67 @@ VALUES ('2020-01-15', 1,  15, 12, 3),
 	--e)
 	
 	--f)
+	SELECT imie, nazwisko, (godziny.liczba_godzin - 160) AS nadgodziny FROM
+	pracownicy, wynagrodzenia, godziny WHERE pracownicy.id_pracownika=wynagrodzenia.id_pracownika AND
+	wynagrodzenia.id_godziny=godziny.id_godziny AND godziny.liczba_godzin > 160;
 	--f)
 	
 	--g)
-	SELECT imie, nazwisko FROM id_pracownika FROM wynagrodzenia INNER JOIN pensja_stanowisko ON wynagrodzenia.id_pensji=pensja_stanowisko.id_pensji where pensja_stanowisko.kwota BETWEEN 1500 AND 3000;
+	SELECT imie, nazwisko FROM pracownicy, wynagrodzenia, pensja_stanowisko WHERE
+	pracownicy.id_pracownika=wynagrodzenia.id_pracownika AND wynagrodzenia.id_pensji=pensja_stanowisko.id_pensji AND
+	pensja_stanowisko.kwota BETWEEN 1500 AND 3000;
 	--g)
 	
 	--h)
+	SELECT imie, nazwisko FROM pracownicy, wynagrodzenia, godziny, premia WHERE
+	pracownicy.id_pracownika=wynagrodzenia.id_pracownika AND wynagrodzenia.id_godziny=godziny.id_godziny AND
+	wynagrodzenia.id_premii=premia.id_premii AND godziny.liczba_godzin > 160 AND premia.rodzaj='brak';
 	--h)
 --6
 
 --7
 	--a)
-	SELECT imie, nazwisko FROM pracownicy INNER JOIN wynagrodzenia ON wynagrodzenia.id_pracownika=pracownicy.id_pracownika INNER JOIN pensja_stanowisko ON wynagrodzenia.id_pensji=pensja_stanowisko.id_pensji ORDER BY pensja_stanowisko.kwota;
+	SELECT imie, nazwisko FROM pracownicy, wynagrodzenia, pensja_stanowisko WHERE
+	wynagrodzenia.id_pracownika=pracownicy.id_pracownika AND wynagrodzenia.id_pensji=pensja_stanowisko.id_pensji 
+	ORDER BY pensja_stanowisko.kwota;
 	--a)
 	
 	--b)
-	SELECT imie, nazwisko FROM pracownicy INNER JOIN wynagrodzenia ON wynagrodzenia.id_pracownika=pracownicy.id_pracownika INNER JOIN pensja_stanowisko ON wynagrodzenia.id_pensji=pensja_stanowisko.id_pensji INNER JOIN premia ON wynagrodzenia.id_premii=premia.id_premii ORDER BY pensja_stanowisko.kwota DESC, premia.kwota DESC;
+	SELECT imie, nazwisko FROM pracownicy, wynagrodzenia, pensja_stanowisko, premia WHERE
+	wynagrodzenia.id_pracownika=pracownicy.id_pracownika AND wynagrodzenia.id_pensji=pensja_stanowisko.id_pensji AND
+	wynagrodzenia.id_premii=premia.id_premii ORDER BY pensja_stanowisko.kwota DESC, premia.kwota DESC;
 	--b)
 	
 	--c)
-	
+	SELECT stanowisko, COUNT(wynagrodzenia.id_pensji) FROM pensja_stanowisko, wynagrodzenia WHERE 
+	wynagrodzenia.id_pensji=pensja_stanowisko.id_pensji GROUP BY pensja_stanowisko.stanowisko;
 	--c)
 	
 	--d)
-	SELECT imie, nazwisko FROM pracownicy WHERE imie LIKE 'J%';
+	SELECT MIN(pensja_stanowisko.kwota), MAX(pensja_stanowisko.kwota), AVG(pensja_stanowisko.kwota) FROM 
+	pensja_stanowisko WHERE pensja_stanowisko.stanowisko='szefunio';
 	--d)
 	
 	--e)
-	SELECT imie, nazwisko FROM pracownicy WHERE imie LIKE '%a' AND nazwisko LIKE '%n%';
+	SELECT SUM(pensja_stanowisko.kwota + premia.kwota) AS suma_wynagrodzen FROM pensja_stanowisko, wynagrodzenia, premia
+	WHERE wynagrodzenia.id_pensji=pensja_stanowisko.id_pensji AND wynagrodzenia.id_premii=premia.id_premii;
 	--e)
 	
 	--f)
+	SELECT stanowisko, SUM(pensja_stanowisko.kwota + premia.kwota) AS suma_wynagrodzen FROM pensja_stanowisko, wynagrodzenia, premia
+	WHERE wynagrodzenia.id_pensji=pensja_stanowisko.id_pensji AND wynagrodzenia.id_premii=premia.id_premii GROUP BY stanowisko;
 	--f)
 	
 	--g)
-	SELECT id_pracownika FROM wynagrodzenia INNER JOIN pensja_stanowisko ON wynagrodzenia.id_pensji=pensja_stanowisko.id_pensji where pensja_stanowisko.kwota BETWEEN 1500 AND 3000;
+	SELECT stanowisko, COUNT(premia.id_premii) AS suma_wynagrodzen FROM pensja_stanowisko, wynagrodzenia, premia
+	WHERE wynagrodzenia.id_pensji=pensja_stanowisko.id_pensji AND wynagrodzenia.id_premii=premia.id_premii GROUP BY stanowisko;
 	--g)
 	
 	--h)
+	DELETE FROM pracownicy WHERE id_pracownika IN 
+	(SELECT pracownicy.id_pracownika FROM pracownicy, wynagrodzenia, pensja_stanowisko WHERE
+	wynagrodzenia.id_pracownika=pracownicy.id_pracownika AND wynagrodzenia.id_pensji=pensja_stanowisko.id_pensji AND 
+	pensja_stanowisko.kwota < 1200);
 	--h)
 --7
 
