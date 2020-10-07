f1b76434249a5cb01c3a9de3c8564c6ac36c0354
Karol Kadziola <karkad96@gmail.com>
2020-10-07 17:43:47 +0200

    liczba_godzin in godziny table now contains worked hours per day instead of worked hours per month, therefore half of codes had to be changed

--- lab-1.sql
+++ lab-1.sql
@@ -99,8 +99,8 @@ GRANT ksiegowosc TO dummy;
 
 --5
 	--a)
-	ALTER TABLE godziny ADD COLUMN miesiac DATE NOT NULL;
-	ALTER TABLE godziny ADD COLUMN numer_tygodnia DATE NOT NULL;
+	ALTER TABLE godziny ADD COLUMN miesiac INTEGER NOT NULL;
+	ALTER TABLE godziny ADD COLUMN numer_tygodnia INTEGER NOT NULL;
 	--a)
 	
 	--b)
@@ -115,59 +115,53 @@ GRANT ksiegowosc TO dummy;
 
 --inserting
 INSERT INTO pracownicy (imie, nazwisko, adres, telefon) 
-VALUES ('Jan',      'Kowalski',   'Wadowicka 19',       '123 343 456'),
-       ('Szczepan', 'Konon',      'Akadamicka 60',      '124 233 857'),
-       ('Karol',    'Nowak',      'Dwornika 13',        '623 740 856'),
-       ('Jaroslaw', 'Polskezbaw', 'Schizofreniczna 69', '720 073 851'),
-       ('Beata',    'Kula',       'Starowislna 23',     '223 343 052'),
-       ('Marek',    'Niezdarek',  'Matecznego 1',       '482 343 459'),
-       ('Pawel',    'Broda',      'Rydlowka 53',        '423 052 450'),
-       ('Alicja',   'Czara',      'Krakowska 109',      '227 343 456'),
-       ('Adam',     'Dlugopis',   'Mickiewicza 6',      '520 343 056'),
-       ('Jessica',  'Jones',      'Mostowa 666',        '923 303 457'),
-       ('Curt',     'Cobain',     'Kaliber 44',         '287 246 728');
-       
-/* 
-	aby dostac numer tygodnia trzeba uzyc: 
-	SELECT DATE_PART('year', AGE((select numer_tygodnia from godziny where id_godziny=<id>), '1753-01-01'));
-	
-	aby dostac miesiac trzeba uzyc:
-	SELECT EXTRACT(MONTH FROM (select miesiac from godziny where id_godziny=<id>));
-*/
-INSERT INTO godziny (data, liczba_godzin, id_pracownika, miesiac, numer_tygodnia) 
-VALUES ('2019-09-10', 115, 2,  '1753-09-01', '1790-01-01'),
-       ('2020-12-17', 167, 3,  '1753-12-01', '1804-01-01'),
-       ('2018-03-06', 125, 10, '1753-03-01', '1763-01-01'),
-       ('2019-01-20', 202, 4,  '1753-01-01', '1756-01-01'),
-       ('2020-02-14', 209, 9,  '1753-02-01', '1760-01-01'),
-       ('2019-11-13', 176, 6,  '1753-11-01', '1799-01-01'),
-       ('2018-05-30', 90,  8,  '1753-05-01', '1775-01-01'),
-       ('2019-10-15', 101, 1,  '1753-10-01', '1795-01-01'),
-       ('2020-06-27', 154, 5,  '1753-06-01', '1779-01-01'),
-       ('2021-04-16', 166, 7,  '1753-04-01', '1753-01-01'),
-       ('2019-08-31', 160, 10, '1753-08-01', '1788-01-01'),
-       ('2020-11-21', 169, 6,  '1753-11-01', '1799-01-01'),
-       ('2020-09-17', 160, 7,  '1753-09-01', '1791-01-01'),
-       ('2019-03-09', 161, 1,  '1753-03-01', '1763-01-01'),
-       ('2020-06-01', 185, 3,  '1753-06-01', '1776-01-01'),
-       ('2019-06-04', 155, 11, '1753-06-01', '1776-01-01');
+VALUES ('Jan',      'Kowalski',   'Wadowicka 19',       '123343456'),
+       ('Szczepan', 'Konon',      'Akadamicka 60',      '124233857'),
+       ('Karol',    'Nowak',      'Dwornika 13',        '623740856'),
+       ('Jaroslaw', 'Polskezbaw', 'Schizofreniczna 69', '720073851'),
+       ('Beata',    'Kula',       'Starowislna 23',     '223343052'), 
+       ('Marek',    'Niezdarek',  'Matecznego 1',       '482343459'),
+       ('Pawel',    'Broda',      'Rydlowka 53',        '423052450'),
+       ('Alicja',   'Czara',      'Krakowska 109',      '227343456'),
+       ('Adam',     'Dlugopis',   'Mickiewicza 6',      '520343056'),
+       ('Jessica',  'Jones',      'Mostowa 666',        '923303457'),
+       ('Curt',     'Cobain',     'Kaliber 44',         '287246728'),
+       ('Monika',   'Worek',      'Workowa 76',         '234267457'),
+       ('Ozzy',     'Osbourne',    'Sabatowa 666',       '783645877'),
+       ('Ian',      'Anderson',   'Flecista 1',         '123768393');
+        
+DO
+$do$
+DECLARE
+   data DATE := '2020-10-01';
+BEGIN 
+   FOR i IN 1..600 LOOP
+   	data := (SELECT DATE((SELECT TIMESTAMP '2020-10-01' + random() * (TIMESTAMP '2020-12-31 20:00:00' - TIMESTAMP '2020-10-01 10:00:00'))));
+	INSERT INTO godziny (data, liczba_godzin, id_pracownika, miesiac, numer_tygodnia)
+	VALUES (data, (SELECT floor(random() * (12-5+1) + 5)::INTEGER), (SELECT floor(random() * (14-1+1) + 1)::INTEGER), 
+	(SELECT EXTRACT(MONTH FROM data)), (SELECT EXTRACT(WEEK FROM data)));
+   END LOOP;
+END
+$do$;
        
 INSERT INTO pensja_stanowisko (stanowisko, kwota) 
-VALUES ('kurier',        999.45),
-       ('ksiegowy',      3500.80),
-       ('szefunio',      10500.66),
-       ('HR',            4555.23),
-       ('programista',   15541.50),
-       ('htmlowiec',     1643.76),
-       ('menadzer',      6231.54),
-       ('bazodanowiec',  544.87),
-       ('admin_sieci',   10505.34),
-       ('admin_sysopow', 340.65),
-       ('sprzatacz',     2340.97),
-       ('kucharz',       3430.23);
+VALUES ('kurier',             999.45),
+       ('ksiegowy',           3500.80),
+       ('kierownik_kadry_HR', 10504.66),
+       ('kierownik_zespolu',  12578.66),
+       ('kierownik_firmy',    14502.66),
+       ('HR',                 4555.23),
+       ('programista',        15541.50),
+       ('htmlowiec',          1643.76),
+       ('menadzer',           6231.54),
+       ('bazodanowiec',       544.87),
+       ('admin_sieci',        10505.34),
+       ('admin_sysopow',      340.65),
+       ('sprzatacz',          2340.97),
+       ('kucharz',            3430.23);
        
 INSERT INTO premia (rodzaj, kwota) 
-VALUES ('nadgodziny',    12.66),
+VALUES ('pomoc_koledze', 12.66),
        ('sprzatanie',    203.53),
        ('bycie_milym',   102.46),
        ('nienarzekanie', 34.18),
@@ -177,19 +171,45 @@ VALUES ('nadgodziny',    12.66),
        ('stanie',        10.12),
        ('lezenie',       12.54),
        ('brak',      	  0);
+
+CREATE TABLE temp (id SERIAL, id_pracownika INTEGER NOT NULL, id_pensji INTEGER NOT NULL);
+
+INSERT INTO temp (id_pracownika, id_pensji) 
+VALUES (1,  11),
+       (2,  5),
+       (3,  10),
+       (4,  3),
+       (5,  4),
+       (6,  9),
+       (7,  12),
+       (8,  2),
+       (9,  6),
+       (10, 14),
+       (11, 8),
+       (12, 1),
+       (13, 13),
+       (14, 7);
+
+DO
+$do$
+DECLARE
+data DATE := '2020-01-01';
+pracownik INTEGER := 0;
+godzina INTEGER := 0;
+pensja INTEGER := 0;
+BEGIN 
+   FOR i IN 1..600 LOOP
+   	data := (SELECT godziny.data from godziny WHERE id_godziny=i);
+   	pracownik := (SELECT id_pracownika from godziny WHERE id_godziny=i);
+   	pensja := (SELECT id_pensji from temp WHERE id_pracownika=pracownik);
+	INSERT INTO wynagrodzenia (data, id_pracownika, id_godziny, id_pensji, id_premii) 
+	VALUES (data, pracownik, i, pensja, (SELECT floor(random() * (10-1+1) + 1)::INTEGER));
+   END LOOP;
+END
+$do$;
+
+DROP TABLE temp;
        
-INSERT INTO wynagrodzenia (data, id_pracownika, id_godziny, id_pensji, id_premii) 
-VALUES ('2020-01-15', 1,  15, 12, 3),
-       ('2020-09-15', 6,  3,  3,  8),
-       ('2019-03-15', 7,  10, 11, 7),
-       ('2019-09-15', 3,  7,  10, 6),
-       ('2019-04-15', 2,  1,  2,  4),
-       ('2020-07-15', 10, 13, 6,  2),
-       ('2019-02-15', 9,  6,  9,  10),
-       ('2020-05-15', 8,  4,  8,  9),
-       ('2020-11-15', 5,  11, 1,  1),
-       ('2020-12-15', 4,  5,  4,  5),
-       ('2020-03-15', 11, 16, 2,  5);
 --inserting
 
 --6
@@ -198,12 +218,12 @@ VALUES ('2020-01-15', 1,  15, 12, 3),
 	--a)
 	
 	--b)
-	SELECT id_pracownika FROM wynagrodzenia, pensja_stanowisko WHERE wynagrodzenia.id_pensji=pensja_stanowisko.id_pensji
+	SELECT DISTINCT id_pracownika FROM wynagrodzenia, pensja_stanowisko WHERE wynagrodzenia.id_pensji=pensja_stanowisko.id_pensji
 	AND pensja_stanowisko.kwota > 1000;
 	--b)
 	
 	--c)
-	SELECT id_pracownika FROM wynagrodzenia, pensja_stanowisko, premia WHERE wynagrodzenia.id_pensji=pensja_stanowisko.id_pensji
+	SELECT DISTINCT id_pracownika FROM wynagrodzenia, pensja_stanowisko, premia WHERE wynagrodzenia.id_pensji=pensja_stanowisko.id_pensji
 	AND wynagrodzenia.id_premii=premia.id_premii AND premia.rodzaj='brak' AND pensja_stanowisko.kwota > 2000;
 	--c)
 	
@@ -216,59 +236,68 @@ VALUES ('2020-01-15', 1,  15, 12, 3),
 	--e)
 	
 	--f)
-	SELECT imie, nazwisko, (godziny.liczba_godzin - 160) AS nadgodziny FROM
-	pracownicy, wynagrodzenia, godziny WHERE pracownicy.id_pracownika=wynagrodzenia.id_pracownika AND
-	wynagrodzenia.id_godziny=godziny.id_godziny AND godziny.liczba_godzin > 160;
+	SELECT imie, nazwisko, miesiac, (SUM(liczba_godzin)-160) AS nadgodziny FROM godziny
+	JOIN pracownicy ON godziny.id_pracownika=pracownicy.id_pracownika 
+	GROUP BY imie, nazwisko, miesiac ORDER BY imie, nazwisko, miesiac;
 	--f)
 	
 	--g)
-	SELECT imie, nazwisko FROM pracownicy, wynagrodzenia, pensja_stanowisko WHERE
+	SELECT DISTINCT imie, nazwisko FROM pracownicy, wynagrodzenia, pensja_stanowisko WHERE
 	pracownicy.id_pracownika=wynagrodzenia.id_pracownika AND wynagrodzenia.id_pensji=pensja_stanowisko.id_pensji AND
 	pensja_stanowisko.kwota BETWEEN 1500 AND 3000;
 	--g)
 	
 	--h)
-	SELECT imie, nazwisko FROM pracownicy, wynagrodzenia, godziny, premia WHERE
-	pracownicy.id_pracownika=wynagrodzenia.id_pracownika AND wynagrodzenia.id_godziny=godziny.id_godziny AND
-	wynagrodzenia.id_premii=premia.id_premii AND godziny.liczba_godzin > 160 AND premia.rodzaj='brak';
+	SELECT imie, nazwisko, miesiac, SUM(liczba_godzin) FROM pracownicy 
+	INNER JOIN wynagrodzenia ON wynagrodzenia.id_pracownika=pracownicy.id_pracownika
+	INNER JOIN godziny ON godziny.id_godziny=wynagrodzenia.id_godziny
+	GROUP BY imie, nazwisko, miesiac HAVING SUM(liczba_godzin) > 160 ORDER BY imie, nazwisko, miesiac;
 	--h)
 --6
 
 --7
 	--a)
-	SELECT imie, nazwisko FROM pracownicy, wynagrodzenia, pensja_stanowisko WHERE
+	SELECT DISTINCT imie, nazwisko, pensja_stanowisko.kwota FROM pracownicy, wynagrodzenia, pensja_stanowisko WHERE
 	wynagrodzenia.id_pracownika=pracownicy.id_pracownika AND wynagrodzenia.id_pensji=pensja_stanowisko.id_pensji 
 	ORDER BY pensja_stanowisko.kwota;
 	--a)
 	
 	--b)
-	SELECT imie, nazwisko FROM pracownicy, wynagrodzenia, pensja_stanowisko, premia WHERE
-	wynagrodzenia.id_pracownika=pracownicy.id_pracownika AND wynagrodzenia.id_pensji=pensja_stanowisko.id_pensji AND
-	wynagrodzenia.id_premii=premia.id_premii ORDER BY pensja_stanowisko.kwota + premia.kwota DESC;
+	SELECT imie, nazwisko, pensja_stanowisko.kwota + SUM(premia.kwota) AS pensja_premia FROM pracownicy	
+	INNER JOIN wynagrodzenia ON wynagrodzenia.id_pracownika=pracownicy.id_pracownika
+	INNER JOIN premia ON premia.id_premii=wynagrodzenia.id_premii
+	INNER JOIN pensja_stanowisko ON pensja_stanowisko.id_pensji=wynagrodzenia.id_pensji
+	GROUP BY imie, nazwisko, pensja_stanowisko.kwota ORDER BY pensja_stanowisko.kwota + SUM(premia.kwota) DESC;
 	--b)
 	
 	--c)
-	SELECT stanowisko, COUNT(wynagrodzenia.id_pensji) FROM pensja_stanowisko, wynagrodzenia WHERE 
-	wynagrodzenia.id_pensji=pensja_stanowisko.id_pensji GROUP BY pensja_stanowisko.stanowisko;
+	SELECT stanowisko, count(stanowisko) FROM pensja_stanowisko, wynagrodzenia WHERE 
+	wynagrodzenia.id_pensji=pensja_stanowisko.id_pensji GROUP BY stanowisko, ;
 	--c)
 	
 	--d)
 	SELECT MIN(pensja_stanowisko.kwota), MAX(pensja_stanowisko.kwota), AVG(pensja_stanowisko.kwota) FROM 
-	pensja_stanowisko WHERE pensja_stanowisko.stanowisko='szefunio';
+	pensja_stanowisko WHERE pensja_stanowisko.stanowisko LIKE '%kierownik%';
 	--d)
 	
 	--e)
-	SELECT SUM(pensja_stanowisko.kwota + premia.kwota) AS suma_wynagrodzen FROM pensja_stanowisko, wynagrodzenia, premia
-	WHERE wynagrodzenia.id_pensji=pensja_stanowisko.id_pensji AND wynagrodzenia.id_premii=premia.id_premii;
+	SELECT SUM(sumy) AS suma_wszystkich_wynagrodzen FROM (SELECT pensja_stanowisko.kwota+premia.kwota*COUNT(premia.rodzaj) AS sumy FROM pracownicy
+	INNER JOIN wynagrodzenia ON wynagrodzenia.id_pracownika=pracownicy.id_pracownika
+	INNER JOIN pensja_stanowisko ON wynagrodzenia.id_pensji=pensja_stanowisko.id_pensji
+	INNER JOIN premia ON wynagrodzenia.id_premii=premia.id_premii
+	GROUP BY premia.rodzaj, premia.kwota, pensja_stanowisko.kwota) AS foo;
 	--e)
 	
 	--f)
-	SELECT stanowisko, SUM(pensja_stanowisko.kwota + premia.kwota) AS suma_wynagrodzen FROM pensja_stanowisko, wynagrodzenia, premia
-	WHERE wynagrodzenia.id_pensji=pensja_stanowisko.id_pensji AND wynagrodzenia.id_premii=premia.id_premii GROUP BY stanowisko;
+	SELECT stanowisko, SUM(sumy) FROM (SELECT stanowisko, pensja_stanowisko.kwota + premia.kwota*COUNT(premia.rodzaj) AS sumy FROM pracownicy
+	INNER JOIN wynagrodzenia ON wynagrodzenia.id_pracownika=pracownicy.id_pracownika
+	INNER JOIN pensja_stanowisko ON wynagrodzenia.id_pensji=pensja_stanowisko.id_pensji
+	INNER JOIN premia ON wynagrodzenia.id_premii=premia.id_premii
+	GROUP BY stanowisko, pensja_stanowisko.kwota, premia.kwota) AS foo GROUP BY stanowisko;
 	--f)
 	
 	--g)
-	SELECT stanowisko, COUNT(premia.id_premii) AS suma_wynagrodzen FROM pensja_stanowisko, wynagrodzenia, premia
+	SELECT stanowisko, COUNT(premia.id_premii) AS liczba_premii FROM pensja_stanowisko, wynagrodzenia, premia
 	WHERE wynagrodzenia.id_pensji=pensja_stanowisko.id_pensji AND wynagrodzenia.id_premii=premia.id_premii GROUP BY stanowisko;
 	--g)
 	
@@ -283,13 +312,13 @@ VALUES ('2020-01-15', 1,  15, 12, 3),
 --8
 	--a)
 	ALTER TABLE pracownicy 
-	ALTER COLUMN telefon TYPE VARCHAR(16);
+	ALTER COLUMN telefon TYPE VARCHAR(17);
 	
-	UPDATE pracownicy SET telefon = CONCAT('(+48)', telefon);
+	UPDATE pracownicy SET telefon = CONCAT('(+48) ', telefon);
 	--a)
 	
 	--b)
-	UPDATE pracownicy SET telefon = REPLACE(telefon, ' ', '-');
+	UPDATE pracownicy SET telefon = CONCAT(LEFT(telefon, 6), SUBSTRING(telefon, 7, 3), '-', SUBSTRING(telefon, 10, 3), '-', RIGHT(telefon, 3));
 	--b)
 	
 	--c)
@@ -299,7 +328,12 @@ VALUES ('2020-01-15', 1,  15, 12, 3),
 	
 	--d)
 	SELECT MD5(imie||nazwisko||adres||telefon||pensja_stanowisko.kwota) FROM pracownicy, pensja_stanowisko, wynagrodzenia WHERE
-	wynagrodzenia.id_pracownika=pracownicy.id_pracownika AND wynagrodzenia.id_pensji=pensja_stanowisko.id_pensji;
+	wynagrodzenia.id_pracownika=pracownicy.id_pracownika AND wynagrodzenia.id_pensji=pensja_stanowisko.id_pensji 
+	GROUP BY pracownicy.id_pracownika, pensja_stanowisko.kwota;
 	--d)
 --8
+
+--9
+
+--9
 
